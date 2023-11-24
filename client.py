7fa9302718cb31d61629b4946a84339a24e1afa3
Anderson Corrêa <deidaraa2002@gmail.com>
2023-11-24 06:56:21 -0300

    .

--- client.py
+++ client.py
@@ -1,11 +1,30 @@
 import socket
+import telebot
+
+
+CHAVE_API = "6680158518:AAG1bJvpEAvf69YJNZol4NrYezfz14e27_Y"
+
+bot = telebot.TeleBot(CHAVE_API)
+
 
 def get_status(query):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('127.0.0.1', 8080))
+    client.connect(('192.168.3.101', 8080))
     client.send(query.encode())
     from_server = client.recv(4096)
     client.close()
-    print (from_server.decode())
-    
-get_status("{\"action\": \"status\"}")
+    return str(from_server.decode())
+
+
+def verificar(mensagem):
+    return True
+
+@bot.message_handler(func=verificar)
+def responder(mensagem):
+    board_return =  get_status("{\"action\": \"status\"}")
+    if board_return is None:
+        board_return = 'Aconteceu um erro interno!'
+    bot.reply_to(mensagem, board_return)
+
+
+bot.polling()
