2d7cde670c4893c653b32b968a0b149d7782689a
Anderson Corrêa <deidaraa2002@gmail.com>
2023-11-25 17:03:08 -0300

    client finished

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import socket
 import telebot
-
+import haddle_static
 
 CHAVE_API = "6680158518:AAG1bJvpEAvf69YJNZol4NrYezfz14e27_Y"
 
 bot = telebot.TeleBot(CHAVE_API)
 
 
-def get_status(query):
+def get_response(query):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('192.168.3.101', 8080))
     client.send(query.encode())
@@ -15,17 +15,32 @@ def get_status(query):
     client.close()
     return str(from_server.decode())
 
+@bot.message_handler(commands=["start"])
+def opcao3(mensagem):
+    board_return =  get_response("{\"action\": \"start_or_restart\"}")
+    if board_return is None:
+        board_return = 'Aconteceu um erro interno!'
+    bot.send_message(mensagem.chat.id, board_return)
+
+
+@bot.message_handler(commands=["status"])
+def opcao3(mensagem):
+    board_return =  get_response("{\"action\": \"status\"}")
+    if board_return is None:
+        board_return = 'Aconteceu um erro interno!'
+    bot.send_message(mensagem.chat.id, board_return)
 
 def verificar(mensagem):
     return True
 
 @bot.message_handler(func=verificar)
 def responder(mensagem):
-    board_return =  get_status("{\"action\": \"status\"}")
-    if board_return is None:
-        board_return = 'Aconteceu um erro interno!'
-    bot.reply_to(mensagem, board_return)
-
+    start_message = """
+    Escolha uma opção para continuar (Clique no item):
+     /start - restart Inicia ou reinicia a lixeira (deve estar vazia)
+     /status Consulta a situação da lixeira
+Responder qualquer outra coisa não vai funcionar, clique ou digite uma das opções"""
+    bot.reply_to(mensagem, start_message)
 
 bot.polling()
 
